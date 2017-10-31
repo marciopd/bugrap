79eeeadb8a264a4dd7514b006fbb88be2b2b9df3
marciodantas <marciodantas@g-10-64-12-85.guest.vaadin.com>
2017-10-31 12:01:55 +0200

    Using Navigator object to navigate and Project as comobobox entity type.

--- src/main/java/com/vaadin/training/bugrap/presenter/MainView.java
+++ src/main/java/com/vaadin/training/bugrap/presenter/MainView.java
@@ -1,27 +1,20 @@
 package com.vaadin.training.bugrap.presenter;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.Set;
 
 import org.vaadin.bugrap.domain.entities.Project;
 
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
-import com.vaadin.server.VaadinServletResponse;
-import com.vaadin.server.VaadinSession;
 import com.vaadin.training.bugrap.model.BugrapFacade;
 import com.vaadin.training.bugrap.view.MainViewDesign;
+import com.vaadin.training.bugrap.view.Views;
 
 public class MainView extends MainViewDesign implements View {
 
 	private static final long serialVersionUID = 6316325087699578693L;
-
-	private static final String APP_HOME_PATH = "/";
-	private static final String MSG_ERROR_REDIRECTING_HOME = "Error redirecting to home after logout.";
 
 	@Override
 	public void enter(final ViewChangeEvent event) {
@@ -30,28 +23,12 @@ public class MainView extends MainViewDesign implements View {
 
 		logoutButton.addClickListener(clickEvent -> {
 			UserController.getInstance().logout();
-			VaadinSession.getCurrent().getSession().invalidate();
-			sendRedirectHome();
+			getUI().getNavigator().navigateTo(Views.LOGIN);
 		});
 
-		final Set<Project> projects = BugrapFacade.getInstance().findProjects();
-		projectSelect.setItems(getProjectNames(projects));
+		final List<Project> projects = new ArrayList<>(BugrapFacade.getInstance().findProjects());
+		Collections.sort(projects);
+		projectSelect.setItems(projects);
 	}
 
-	private void sendRedirectHome() {
-		try {
-			VaadinServletResponse.getCurrent().sendRedirect(APP_HOME_PATH);
-		} catch (final IOException e) {
-			throw new IllegalStateException(MSG_ERROR_REDIRECTING_HOME, e);
-		}
-	}
-
-	private Collection<String> getProjectNames(final Collection<Project> projects) {
-		final List<String> names = new ArrayList<>();
-		for (final Project project : projects) {
-			names.add(project.getName());
-		}
-		Collections.sort(names);
-		return names;
-	}
 }
